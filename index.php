25e4d60bd5e2094b54f299ea6f7f92fa09a97569
Stephen Sabatini <info@stephensabatini.com>
2020-04-28 19:03:49 -0400

    Initial commit.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="https://schema.org/WebPage">
+    <head>
+        <meta charset="UTF-8">
+        <title>Untitled</title>
+        <meta name="description" content="">
+        <link rel="stylesheet" href="dist/css/index.min.css">
+        <script src="dist/js/index.min.js" async></script>
+        <!-- Copy Google Analytics here. -->
+    </head>
+    <body>
+        <div class="site-wrapper">
+            <header id="header" class="site-header" role="banner" itemscope itemtype="https://schema.org/WPHeader">
+                <h1>Site Name</h1>
+                <button id="nav-toggle" class="site-nav-toggle" aria-controls="nav" aria-expanded="false">☰</button>
+                <nav id="nav" class="site-nav" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement" aria-label="Main">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main id="main" class="site-main" role="main" itemscope itemtype="https://schema.org/WebPageElement" itemprop="mainContentOfPage">
+                <article>
+                    <h2>Untitled</h2>
+                    <p></p>
+                </article>
+            </main>
+            <aside id="sidebar" class="site-sidebar" role="complementary" itemscope itemtype="https://schema.org/WPSidebar">
+            </aside>
+            <footer id="footer" class="site-footer" role="contentinfo" itemscope itemtype="https://schema.org/WPFooter">
+                &copy; Site Name. All rights reserved.
+            </footer>
+        </div>
+    </body>
+</html>
